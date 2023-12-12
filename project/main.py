7487aab7d3325cfc8e884556f34e6e8f84945961
Aidar <aydar.zam05@gmail.com>
2023-12-12 16:47:05 +0300

    Hid token

--- project/main.py
+++ project/main.py
@@ -1,9 +1,13 @@
 import telebot
+import os
+from dotenv import load_dotenv, find_dotenv
 from start import *
 from button_operation import *
 from work_with_notes import *
 from info import *
 
-bot = telebot.TeleBot('6600265888:AAF1YKbFqVrw_T2lmYY3DVRV2qyTg2PnTx0')
+load_dotenv(find_dotenv())
+
+bot = telebot.TeleBot(os.getenv('TOKEN'))
 
 bot.polling(none_stop=True)
